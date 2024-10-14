8918d07433ff53dda879bd1345e35fc8984ca2b8
Chucklesthehacker <brynctiller@gmail.com>
2024-10-14 17:45:00 +1100

    PyCharm file

--- Assignment3.py
+++ Assignment3.py
@@ -19,18 +19,42 @@ from sklearn.metrics import mean_absolute_error
 
 import joblib
 
-gold_prices = pd.read_csv('FINAL_USO.csv')
-
+gold_data = pd.read_csv('FINAL_USO.csv')
 def clean_dataset(data):
     # Step 1: convert columns to datetime where possible, keep original value
-    for col in data.columns:
+    for col in data.select_dtypes(include=['object']).columns:
         try:
             data[col] = pd.to_numeric(data[col])
         except:
-            data[col]=pd.to_datetime(data[col])
+            data[col] = data[col]
+
+    for col in data.select_dtypes(include=['object']).columns:
+        try:
+            data[col] = pd.to_datetime(data[col])
+        except:
+            data[col] = data[col]
+
+    # if col in data.columns.all() == 0:
+    #     data[col] = data[col].astype('bool')
+    # elif col in data.columns.all()  == 1:
+    #     data[col] = data[col].astype('bool')
+    # else:
+    #     data[col] = data[col]
+
+st.sidebar.title('Select Dataset(s)')
+uploaded_file = st.sidebar.file_uploader("Choose one or more CSV files", type=['csv'], accept_multiple_files=True)
+
+datasets = {}
+
+if uploaded_file:
+    for uploaded_files in uploaded_file:
+        datasets[uploaded_file.name] = pd.read_csv(uploaded_file)
+
+    selected_dataset = st.sidebar.selectbox('Select Dataset', options=list(datasets.keys()))
+
+    data = datasets[selected_dataset]
+    cleaned_data = data.drop_duplicates(keep='first')
 
 
-
-clean_dataset(gold_prices)
-
-gold_prices.info()
+# gold_data.drop_duplicates(keep='first')
+# print(gold_data.shape)
